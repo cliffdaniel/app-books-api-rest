395fb6940ea40ecf5f458e112ca99d5cfb296017
Daniel Arenas <jdaniel.arenas@gmail.com>
2025-01-10 14:16:35 -0500

    chore: add linter & prettier config

---
+++ eslint.config.js
@@ -0,0 +1,71 @@
+const eslintNestJs = require('eslint-config-nestjs');
+const tseslint = require('@typescript-eslint/eslint-plugin');
+const tsParser = require('@typescript-eslint/parser');
+const prettier = require('eslint-config-prettier');
+const importPlugin = require('eslint-plugin-import');
+const unusedImports = require('eslint-plugin-unused-imports');
+
+module.exports = [
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: ['./tsconfig.eslint.json'],
+        sourceType: 'module'
+    }
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+      import: importPlugin,
+      prettier,
+      'unused-imports': unusedImports,
+    },
+    rules: {
+      'import/order': [
+        'error',
+        {
+          groups: [['builtin', 'external'], 'internal', ['parent', 'sibling', 'index']],
+          'newlines-between': 'always',
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+        },
+      ],
+      'import/no-unresolved': 'off',
+      'import/namespace': 'off',
+      'import/no-duplicates': 'off',
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { varsIgnorePattern: '^_', argsIgnorePattern: '^_' },
+      ],
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'warn',
+        { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' },
+      ],
+    },
+  },
+  {
+    // Configuración adicional que se necesita sin 'extends'
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: './tsconfig.eslint.json',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+      import: importPlugin,
+      prettier,
+      'unused-imports': unusedImports,
+    },
+    rules: {
+      ...eslintNestJs.rules, // Incluir reglas de eslint-config-nestjs
+      ...prettier.rules, // Incluir reglas de eslint-config-prettier
+    },
+  },
+];
